f244d948a0cfb2dc16676a96b3e2dd8548dbcf89
laur73 <jj.c.r.1781@gmail.com>
2023-02-27 14:41:19 -0600

    Endpoints de paciente creados

---
+++ backend/routes/pacienteRoutes.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import {
+	agregarPaciente,
+	obtenerPacientes,
+	obtenerPaciente,
+	actualizarPaciente,
+	eliminarPaciente
+}
+	from '../controllers/pacienteController.js';
+
+import checkAuth from '../middleware/authMiddleware.js';
+
+const router = express.Router();
+
+router.post('/', checkAuth, agregarPaciente);
+
+router.get('/', checkAuth, obtenerPacientes);
+
+router.get('/:id', checkAuth, obtenerPaciente);
+
+router.put('/:id', checkAuth, actualizarPaciente);
+
+router.delete('/:id', checkAuth, eliminarPaciente);
+
+
+export default router;
