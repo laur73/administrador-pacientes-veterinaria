56941829165b3bcb4743dc646e8e17f8c9e7f85f
laur73 <jj.c.r.1781@gmail.com>
2023-02-28 11:09:48 -0600

    Páginas para el layout de auntenticacion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthLayout from "./layout/AuthLayout";
+import Login from "./pages/Login";
+import ConfirmarCuenta from "./pages/ConfirmarCuenta";
+import OlvidePassword from "./pages/OlvidePassword";
+import Registrar from "./pages/Registrar";
+
 function App() {
-    return <h1>Hola Mundo</h1>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<AuthLayout />}>
+					<Route index element={<Login />} />
+					<Route path="registrar" element={<Registrar />} />
+					<Route path="olvide-password" element={<OlvidePassword />} />
+					<Route path="confirmar/:id" element={<ConfirmarCuenta />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ frontend/src/pages/OlvidePassword.jsx
@@ -0,0 +1,9 @@
+const OlvidePassword = () => {
+	return (
+		<>
+			<h1>Desde Olvidé Password</h1>
+		</>
+	);
+};
+
+export default OlvidePassword;
