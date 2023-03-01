e79497c27f9746addc5eb38d14954632692e4eef
laur73 <jj.c.r.1781@gmail.com>
2023-03-01 11:14:41 -0600

    Formulario HTML/CSS Listo

--- frontend/src/pages/OlvidePassword.jsx
+++ frontend/src/pages/OlvidePassword.jsx
@@ -1,7 +1,31 @@
+import { Link } from "react-router-dom";
+
 const OlvidePassword = () => {
 	return (
 		<>
-			<h1>Desde Olvidé Password</h1>
+			<div>
+				<h1 className="text-indigo-600 font-black text-5xl text-center">¿Olvidaste tu Contraseña? <span className="text-black">¡Genera una Nueva!</span></h1>
+			</div>
+
+			<div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
+				<form>
+
+					<div className="my-5">
+						<label className="uppercase text-gray-600 block text-xl bg- font-bold mb-3">Correo Electrónico</label>
+						<input type="email" placeholder="Escribe tu correo electrónico..." className="border w-full p-3 bg-gray-50 rounded-md" />
+					</div>
+
+					<div className="my-5 flex justify-center">
+						<input type="submit" value="Enviar Enlace " className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:full " />
+					</div>
+
+				</form>
+
+				<nav className="lg:flex lg:justify-evenly">
+					<Link to="/" className="block text-center my-5 text-gray-500">¿Ya tienes una cuenta? Inicia Sesión</Link>
+				</nav>
+			</div>
+
 		</>
 	);
 };
