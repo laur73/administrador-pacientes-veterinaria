14fce3dc7457e088f634b437a926817a3cc07019
laur73 <jj.c.r.1781@gmail.com>
2023-03-03 12:52:17 -0600

    Componente listo

--- frontend/src/pages/OlvidePassword.jsx
+++ frontend/src/pages/OlvidePassword.jsx
@@ -1,6 +1,31 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import Alerta from "../components/Alerta";
+import clienteAxios from "../config/axios";
 
 const OlvidePassword = () => {
+
+	const [email, setEmail] = useState('');
+	const [alerta, setAlerta] = useState({});
+
+	const handleSubmit = async e => {
+		e.preventDefault();
+
+		if (email === '') {
+			setAlerta({ msg: 'Debes escribir un correo electrónico', error: true })
+			return;
+		}
+
+		try {
+			const { data } = await clienteAxios.post('/veterinarios/recuperar-password', { email })
+			setAlerta({ msg: data.msg })
+		} catch (error) {
+			setAlerta({ msg: error.response.data.msg, error: true })
+		}
+	}
+
+	const { msg } = alerta;
+
 	return (
 		<>
 			<div>
@@ -8,11 +33,14 @@ const OlvidePassword = () => {
 			</div>
 
 			<div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
-				<form>
+
+				{msg && <Alerta alerta={alerta} />}
+
+				<form onSubmit={handleSubmit}>
 
 					<div className="my-5">
 						<label className="uppercase text-gray-600 block text-xl bg- font-bold mb-3">Correo Electrónico</label>
-						<input type="email" placeholder="Escribe tu correo electrónico..." className="border w-full p-3 bg-gray-50 rounded-md" />
+						<input type="email" placeholder="Escribe tu correo electrónico..." className="border w-full p-3 bg-gray-50 rounded-md" value={email} onChange={e => setEmail(e.target.value)} />
 					</div>
 
 					<div className="my-5 flex justify-center">
