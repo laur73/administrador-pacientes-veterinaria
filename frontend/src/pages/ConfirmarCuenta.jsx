76985d2e346a51532f35da83c3a4d416b5a4e585
laur73 <jj.c.r.1781@gmail.com>
2023-03-03 12:52:07 -0600

    Componente listo

--- frontend/src/pages/ConfirmarCuenta.jsx
+++ frontend/src/pages/ConfirmarCuenta.jsx
@@ -1,7 +1,47 @@
+import { useParams, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import clienteAxios from "../config/axios";
+import Alerta from "../components/Alerta";
+
 const ConfirmarCuenta = () => {
+
+	const [cuentaConfirmada, setCuentaConfirmada] = useState(false);
+	const [cargando, setCargando] = useState(true);
+	const [alerta, setAlerta] = useState({});
+
+	const params = useParams();
+	const { id } = params;
+
+	useEffect(() => {
+		const confirmarCuenta = async () => {
+			try {
+				const url = `/veterinarios/confirmar/${id}`;
+				const { data } = await clienteAxios(url);
+
+				setCuentaConfirmada(true);
+				setAlerta({ msg: data.msg })
+			} catch (error) {
+				setAlerta({ msg: error.response.data.msg, error: true })
+			}
+
+			setCargando(false);
+		}
+		confirmarCuenta();
+	}, [])
+
 	return (
 		<>
-			<h1>Desde Confirmar Cuenta</h1>
+			<div>
+				<h1 className="text-indigo-600 font-black text-5xl text-center">Confirma tu Cuenta y Administra tus <span className="text-black">Pacientes</span></h1>
+			</div>
+
+			<div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
+				{!cargando && <Alerta alerta={alerta} />}
+
+				{cuentaConfirmada && (
+					<Link to="/" className="flex justify-center bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-full">Inicia Sesión</Link>
+				)}
+			</div>
 		</>
 	);
 };
