34ed7630d5d837e3e08c97692dd3f37028ea4c26
laur73 <jj.c.r.1781@gmail.com>
2023-03-03 22:10:58 -0600

    Creacion del Context Provider

---
+++ frontend/src/context/AuthProvider.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect, createContext } from 'react'
+import clienteAxios from '../config/axios';
+
+const AuthContext = createContext();
+
+//Componente padre que va a tener todos los componentes de la app
+const AuthProvider = ({ children }) => {
+
+	const [auth, setAuth] = useState({})
+
+	useEffect(() => {
+		const autenticarUsuario = async () => {
+			const token = localStorage.getItem('token');
+			if (!token) return;
+
+			const config = {
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${token}`
+				}
+			}
+
+			try {
+				const { data } = await clienteAxios('/veterinarios/perfil', config)
+				setAuth(data);
+			} catch (error) {
+				console.log(error.response.data.msg)
+				setAuth({})
+			}
+		}
+		autenticarUsuario();
+	})
+
+	return (
+		<AuthContext.Provider value={{ auth, setAuth }}>
+			{children}
+		</AuthContext.Provider>
+	)
+}
+
+export {
+	AuthProvider
+}
+
+export default AuthContext
