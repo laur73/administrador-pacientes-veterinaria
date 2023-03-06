207bb70852f5300fffe2b06483122c63a6ccddd9
laur73 <jj.c.r.1781@gmail.com>
2023-03-06 14:23:51 -0600

    Rutas privadas protegidas

--- frontend/src/layout/RutaProtegida.jsx
+++ frontend/src/layout/RutaProtegida.jsx
@@ -1,10 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
 const RutaProtegida = () => {
+
+	const { auth, cargando } = useAuth();
+	console.log(auth);
+	console.log(cargando)
+
+	if (cargando) return 'Cargando...'
+
 	return (
 		<>
 			<h1>Layout de las rutas protegidas</h1>
-			<Outlet />
+
+			{auth?._id ? <Outlet /> : <Navigate to="/" />}
 
 		</>
 	)
