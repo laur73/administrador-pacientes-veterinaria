d9068844b6413afe608e297140f48069b27e25af
laur73 <jj.c.r.1781@gmail.com>
2023-03-07 10:10:41 -0600

    Componente creado

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom'
+import useAuth from '../hooks/useAuth'
+
+const Header = () => {
+
+	const { cerrarSesion } = useAuth();
+
+	return (
+		<header className="py-10 px-10 bg-indigo-600">
+
+			<div className="container mx-auto flex flex-col lg:flex-row justify-between items-center">
+
+				<h1 className="font-bold text-2xl text-center text-white">Administrador de Pacientes de <span className="text-black">Veterinaria</span>
+				</h1>
+
+				<nav className="flex flex-col lg:flex-row gap-4 items-center mt-6 lg:mt-0">
+					<Link to="/admin" className="text-white text-lg font-bold">Pacientes</Link>
+					<Link to="/admin/perfil" className="text-white text-lg font-bold">Perfil</Link>
+
+					<button type="button" className="text-white text-lg font-bold" onClick={cerrarSesion}>Cerrar Sesión</button>
+				</nav>
+
+			</div>
+
+
+		</header>
+	)
+}
+
+export default Header
