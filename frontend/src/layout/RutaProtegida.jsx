bf6c7bc2c6a700cfa92f4b36d757359efa45dc2c
laur73 <jj.c.r.1781@gmail.com>
2023-03-07 10:11:12 -0600

    Se agregaron los componentes H y F

--- frontend/src/layout/RutaProtegida.jsx
+++ frontend/src/layout/RutaProtegida.jsx
@@ -1,19 +1,26 @@
 import { Outlet, Navigate } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import useAuth from "../hooks/useAuth";
 
 const RutaProtegida = () => {
 
 	const { auth, cargando } = useAuth();
-	console.log(auth);
-	console.log(cargando)
+
 
 	if (cargando) return 'Cargando...'
 
 	return (
 		<>
-			<h1>Layout de las rutas protegidas</h1>
+			<Header />
 
-			{auth?._id ? <Outlet /> : <Navigate to="/" />}
+			{auth?._id ? (
+				<main className="container mx-auto mt-10">
+					<Outlet />
+				</main>
+			) : <Navigate to="/" />}
+
+			<Footer />
 
 		</>
 	)
