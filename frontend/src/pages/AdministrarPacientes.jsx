40c339738b8f70d89e40a4517250b99f6a245173
laur73 <jj.c.r.1781@gmail.com>
2023-03-07 21:16:00 -0600

    Contenedor de formulario y listado listo

--- frontend/src/pages/AdministrarPacientes.jsx
+++ frontend/src/pages/AdministrarPacientes.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react"
+import Formulario from "../components/Formulario"
+import ListadoPacientes from "../components/ListadoPacientes"
+
 const AdministrarPacientes = () => {
+
+	const [mostrarFormulario, setMostrarFormulario] = useState(false)
+
 	return (
-		<>
-			<h1>Desde administrar los pacientes</h1>
-		</>
+		<div className="flex flex-col lg:flex-row px-10 gap-10">
+
+			<button
+				type="button" className="bg-indigo-600 text-white font-bold uppercase mx-10 p-3 rounded-md md:hidden" onClick={() => setMostrarFormulario(!mostrarFormulario)}>{!mostrarFormulario ? 'Mostrar Formulario' : 'Ocultar Formulario'}
+			</button>
+
+			<div className={`${mostrarFormulario ? 'block' : 'hidden'} md:block lg:w-1/2 xl:w-2/5`}>
+				<Formulario />
+			</div>
+
+			<div className="lg:w-1/2 xl:w-3/5">
+				<ListadoPacientes />
+			</div>
+		</div>
 	)
 }
 
