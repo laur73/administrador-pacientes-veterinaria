75539ce6785da10c58120a37a3aa2c68a7d47a53
laur73 <jj.c.r.1781@gmail.com>
2023-03-08 12:15:18 -0600

    Funciones para actualizar perfil y password listas

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -41,8 +41,67 @@ const AuthProvider = ({ children }) => {
 		setAuth({});
 	}
 
+	const actualizarPerfil = async datos => {
+		const token = localStorage.getItem('token')
+		if (!token) {
+			setCargando(false)
+			return
+		}
+		const config = {
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${token}`
+			}
+		}
+
+		try {
+			const url = `/veterinarios/perfil/${datos._id}`
+			await clienteAxios.put(url, datos, config)
+
+			return {
+				msg: 'Almacenado Correctamente'
+			}
+		} catch (error) {
+			return {
+				msg: error.response.data.msg,
+				error: true
+			}
+		}
+	}
+
+	const guardarPassword = async (datos) => {
+		const token = localStorage.getItem('token')
+		if (!token) {
+			setCargando(false)
+			return
+		}
+		const config = {
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${token}`
+			}
+		}
+
+		try {
+			const url = '/veterinarios/actualizar-password'
+
+			const { data } = await clienteAxios.put(url, datos, config)
+			console.log(data)
+
+			return {
+				msg: data.msg
+			}
+		} catch (error) {
+			return {
+				msg: error.response.data.msg,
+				error: true
+			}
+		}
+
+	}
+
 	return (
-		<AuthContext.Provider value={{ auth, setAuth, cargando, cerrarSesion }}>
+		<AuthContext.Provider value={{ auth, setAuth, cargando, cerrarSesion, actualizarPerfil, guardarPassword }}>
 			{children}
 		</AuthContext.Provider>
 	)
