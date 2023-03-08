a80984a642eddbb1fc5632768d9de6dca833252b
laur73 <jj.c.r.1781@gmail.com>
2023-03-07 21:12:51 -0600

    Se agregó PacientesProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,24 +13,27 @@ import AdministrarPacientes from "./pages/AdministrarPacientes";
 import Perfil from "./pages/Perfil";
 
 import { AuthProvider } from "./context/AuthProvider";
+import { PacientesProvider } from "./context/PacientesProvider";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<AuthProvider>
-				<Routes>
-					<Route path="/" element={<AuthLayout />}>
-						<Route index element={<Login />} />
-						<Route path="registrar" element={<Registrar />} />
-						<Route path="olvide-password" element={<OlvidePassword />} />
-						<Route path="olvide-password/:token" element={<NuevoPassword />} />
-						<Route path="confirmar/:id" element={<ConfirmarCuenta />} />
-					</Route>
-					<Route path="/admin" element={<RutaProtegida />}>
-						<Route index element={<AdministrarPacientes />} />
-						<Route path="perfil" element={<Perfil />} />
-					</Route>
-				</Routes>
+				<PacientesProvider>
+					<Routes>
+						<Route path="/" element={<AuthLayout />}>
+							<Route index element={<Login />} />
+							<Route path="registrar" element={<Registrar />} />
+							<Route path="olvide-password" element={<OlvidePassword />} />
+							<Route path="olvide-password/:token" element={<NuevoPassword />} />
+							<Route path="confirmar/:id" element={<ConfirmarCuenta />} />
+						</Route>
+						<Route path="/admin" element={<RutaProtegida />}>
+							<Route index element={<AdministrarPacientes />} />
+							<Route path="perfil" element={<Perfil />} />
+						</Route>
+					</Routes>
+				</PacientesProvider>
 			</AuthProvider>
 		</BrowserRouter>
 	);
